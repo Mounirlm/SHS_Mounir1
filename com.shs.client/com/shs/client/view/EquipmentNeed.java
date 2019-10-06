292c86e51dd3286b4d0c7912d90902172473c896
Mounir LEMHARZI <mounpulco@gmail.com>
2019-10-06 17:35:36 +0200

    equipment need

---
+++ com.shs.client/com/shs/client/view/EquipmentNeed.java
@@ -0,0 +1,35 @@
+package com.shs.client.view;
+
+import javax.swing.JPanel;
+
+public class EquipmentNeed extends JPanel {
+/*  
+     private JTextField messageText;
+     private JButton sendButton;
+     private chatMessageTableModel tableModel;
+     
+     public void init(){
+        buildFrame();
+     }*/
+}
+/*
+private void buildFrame(){
+    setLayout(new MigLayout("", "[][grow]", "[][grow]"));
+    add(new JLabel("Message: "), "cell 0 0");
+    messageText = new JTextField();
+    add(messageText, "cell 1 0, grow");
+    sendButton = new JButton("Send");
+    add(sendButton, "cell 2 0");
+    table = new JXTable(); 
+    tableModel = new ChatMessageTableModel(); 
+    table.setModel(tableModel); 
+    table.setCellSelectionEnabled(false); 
+    table.setRowSelectionAllowed(false); 
+    table.setColumnSelectionAllowed(false); 
+    table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); 
+    table.setColumnControlVisible(true); 
+    table.setFillsViewportHeight(true); 
+    table.setFocusable(false); 
+    add(new JScrollPane(table), "cell 0 1 3 1,grow"); 
+}
+*/
