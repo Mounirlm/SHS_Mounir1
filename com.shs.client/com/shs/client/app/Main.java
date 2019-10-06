697c81f0757044c2f1cc91e9219213d54a8a7db9
Mounir LEMHARZI <mounpulco@gmail.com>
2019-10-06 14:43:14 +0200

    main

---
+++ com.shs.client/com/shs/client/app/Main.java
@@ -0,0 +1,15 @@
+package com.shs.client.app;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
+		Application app = new Application();
+		app.start();
+		
+
+	}
+
+}
