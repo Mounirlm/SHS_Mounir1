be986a6353e677a5dbe1f2a683425ba5f1799d84
Mounir LEMHARZI <mounpulco@gmail.com>
2019-10-19 21:13:32 +0200

    adding method update and create sensor

--- com.shs.client/com/shs/client/controller/SensorController.java
+++ com.shs.client/com/shs/client/controller/SensorController.java
@@ -2,11 +2,13 @@ package com.shs.client.controller;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.shs.commons.model.Sensor;
 import com.shs.commons.model.SensorClientHandler;
+import com.shs.commons.model.Type_Sensor;
 
 public class SensorController {
 private SensorClientHandler sensorService;
@@ -27,4 +29,20 @@ public List<Sensor> getAllSensorsWithPosition()throws IOException {
 public ArrayList<Sensor> getSensorsInRoom(int idFloor)throws IOException {
 	  return sensorService.selectSensorInRoom(idFloor);
 }
+
+public  ArrayList<Sensor> getSensorsNotInstalled()throws IOException {
+	return sensorService.selectSensorsNotInstalled();	
+}
+public String update(Sensor s ) throws IOException, SQLException {
+	return sensorService.up_dateSensorToServer(s);
+}
+
+public String create(Sensor s ) throws IOException, SQLException {
+	return sensorService.insertSensorInStockToServer(s);
+}
+
+public String delete(Sensor s ) throws IOException, SQLException {
+	return sensorService.deleteSensor(s);
+}
+
 }
