25c2243b1868c9c47138ea53027cfd4b7c6a0eb4
Mounir LEMHARZI <mounpulco@gmail.com>
2019-10-19 21:33:23 +0200

    Select from TypeSensor didn't work because it was not instantiate

--- com.shs.client/com/shs/client/controller/TypeSensorController.java
+++ com.shs.client/com/shs/client/controller/TypeSensorController.java
@@ -10,6 +10,7 @@ import com.shs.commons.model.SensorClientHandler;
 import com.shs.commons.model.Type_Sensor;
 import com.shs.server.connection.pool.DataSource;
 import com.shs.server.model.SensorTypeManager;
+import com.shs.server.model.Type_SensorManager;
 
 public class TypeSensorController {
 	
@@ -17,8 +18,10 @@ private SensorTypeManager typeSensorService;
 
 private SensorClientHandler typeSensor;
 
+
 public TypeSensorController() throws UnknownHostException, IOException, SQLException, ClassNotFoundException {
 	typeSensorService = new SensorTypeManager(new DataSource().getConnection());
+	typeSensor = new SensorClientHandler();
 }
 
 public boolean update(Type_Sensor typeSensor) throws IOException, SQLException {
@@ -29,9 +32,5 @@ public  List<Type_Sensor> getSensorType()throws IOException, SQLException {
 	return typeSensor.selectAllTypeSensors();
 }
 
-public Type_Sensor getSensorType(String name) throws IOException {
-	
-	return typeSensor.selectSensorType(name);
-}
 
 }
