b98f232ef5b06da6f142be1399085f69859dba12
Mounir LEMHARZI <mounpulco@gmail.com>
2019-10-19 21:23:36 +0200

    adding interface IUpdatable that will be call to update JList in
    MapSHS

--- com.shs.client/com/shs/client/view/IUpdatable.java
+++ com.shs.client/com/shs/client/view/IUpdatable.java
@@ -1,9 +1,12 @@
 package com.shs.client.view;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface IUpdatable {
 
 
-	public void update();
+	public void update() ;
 
 
 }
