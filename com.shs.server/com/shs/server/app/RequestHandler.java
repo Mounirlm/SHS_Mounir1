6aa192f5a43d0a456c862feb33d12ba652a00035
Mounir LEMHARZI <mounpulco@gmail.com>
2019-11-03 18:57:00 +0100

    no change

--- com.shs.server/com/shs/server/app/RequestHandler.java
+++ com.shs.server/com/shs/server/app/RequestHandler.java
@@ -182,17 +182,17 @@ public class RequestHandler implements Runnable {
 			message=reqType_Sensor.requestManager();
 			break;
 
-		case "Historical":
-			Historical historic =(Historical) object;			
-			//add to DB
-			HistoricalRequestManager reqHist = new HistoricalRequestManager(connDB, reader, writer, historic, "insert-Historical");
-			message=reqHist.requestManager();
-			//CACHE
-			synchronized (CACHE) {
-				this.addToCacheAndTreatment(historic);
-			}
-
-			break;
+//		case "Historical":
+//			Historical historic =(Historical) object;			
+//			//add to DB
+//			HistoricalRequestManager reqHist = new HistoricalRequestManager(connDB, reader, writer, historic, "insert-Historical");
+//			message=reqHist.requestManager();
+//			//CACHE
+//			synchronized (CACHE) {
+//				this.addToCacheAndTreatment(historic);
+//			}
+//
+//			break;
 		case "Resident":
 			Resident resident =(Resident) object;
 			ResidentRequestManager reqResident = new ResidentRequestManager(connDB, reader, writer, resident, request);
@@ -223,205 +223,205 @@ public class RequestHandler implements Runnable {
 	/*
 	 * Check if the signal(historical receive) had a message value upper or lower than trigger points of alerts
 	 */
-	private boolean isAlertInCache(Historical historic, Sensor sensor) {
-		boolean rep = false;
-		if (sensor.getFk_type_sensor().getName().equals("door_sensor") || sensor.getFk_type_sensor().getName().equals("window_sensor")) {
-			Time hour_min= new Time(60*60*1000*sensor.getFk_type_sensor().getTrigger_point_min());
-			Time hour_max= new Time(60*60*1000*sensor.getFk_type_sensor().getTrigger_point_max());
-			
-			//Check if the hour of today is upper or lower than trigger hours
-			if ((todayTime().getTime()<= hour_min.getTime()) && (Integer.parseInt(historic.getMessage())==1)) {
-				rep=true;
-			}
-			if ((todayTime().getTime()>= hour_max.getTime()) && (Integer.parseInt(historic.getMessage())==1)) {
-				rep=true;
-			}
-		}else {//temperature, fall and smoke sensors
-			//check if value lower than trigger point min
-			if (sensor.getFk_type_sensor().getTrigger_point_min()!=0) {
-				if (Integer.parseInt(historic.getMessage())<= sensor.getFk_type_sensor().getTrigger_point_min()) {
-					rep=true;
-				}
-			}
-			//check if value upper than max trigger
-			else if (sensor.getFk_type_sensor().getTrigger_point_max()!=0) {
-				if (Integer.parseInt(historic.getMessage())>= sensor.getFk_type_sensor().getTrigger_point_max()) {
-					rep=true;
-				}
-			}
-		}
-		return rep;
-
-	}
+//	private boolean isAlertInCache(Historical historic, Sensor sensor) {
+//		boolean rep = false;
+//		if (sensor.getFk_type_sensor().getName().equals("door_sensor") || sensor.getFk_type_sensor().getName().equals("window_sensor")) {
+//			Time hour_min= new Time(60*60*1000*sensor.getFk_type_sensor().getTrigger_point_min());
+//			Time hour_max= new Time(60*60*1000*sensor.getFk_type_sensor().getTrigger_point_max());
+//			
+//			//Check if the hour of today is upper or lower than trigger hours
+//			if ((todayTime().getTime()<= hour_min.getTime()) && (Integer.parseInt(historic.getMessage())==1)) {
+//				rep=true;
+//			}
+//			if ((todayTime().getTime()>= hour_max.getTime()) && (Integer.parseInt(historic.getMessage())==1)) {
+//				rep=true;
+//			}
+//		}else {//temperature, fall and smoke sensors
+//			//check if value lower than trigger point min
+//			if (sensor.getFk_type_sensor().getTrigger_point_min()!=0) {
+//				if (Integer.parseInt(historic.getMessage())<= sensor.getFk_type_sensor().getTrigger_point_min()) {
+//					rep=true;
+//				}
+//			}
+//			//check if value upper than max trigger
+//			else if (sensor.getFk_type_sensor().getTrigger_point_max()!=0) {
+//				if (Integer.parseInt(historic.getMessage())>= sensor.getFk_type_sensor().getTrigger_point_max()) {
+//					rep=true;
+//				}
+//			}
+//		}
+//		return rep;
+//
+//	}
 
 	/*
 	 * Add the signal value in hash map of cache with id sensor in key and historical in value
 	 * Check if nb_alert signal is reach
 	 * Insert an alert in DB if it is the case
 	 */
-	private void addToCacheAndTreatment(Historical historic) {
-		//get sensor of signal
-		SensorManager sensM = new SensorManager(connDB);
-		Sensor sensor = null;
-		boolean isAlert=false;
-
-		try {//get the sensor of the signal
-			sensor = SensorManager.getSensor(historic.getFk_sensor());
-		} catch (SQLException | ParseException e) {
-			e.printStackTrace();
-		}
-		//add to cache evry signals
-		if (!CACHE.containsKey(historic.getFk_sensor())) {
-			ArrayList<Historical> a  = new ArrayList<Historical>();
-			a.add(historic);
-			CACHE.put(historic.getFk_sensor(), a);
-		}else {
-			ArrayList<Historical> a = CACHE.get(historic.getFk_sensor());
-			a.add(historic);
-		}
-
-		//check of cache to add alert in DB
-		ArrayList<Historical> hitoricals = CACHE.get(sensor.getId());//get all signals of the sensor
-		ArrayList<Historical> last = new ArrayList<>();
-
-		//get last signals
-		for (int i = hitoricals.size()-1; i+1 >= sensor.getFk_type_sensor().getNb_alerts(); i--) {
-			last.add(hitoricals.get(i));
-		}
-
-		//check if we have a number of signals of alert of alert high
-		int cpt_alert_signals = 0;
-		for (int i = 0; i < last.size(); i++) {
-			if (isAlertInCache(last.get(i), sensor)) {
-				cpt_alert_signals++;
-			}
-		}
-		if (cpt_alert_signals==sensor.getFk_type_sensor().getNb_alerts()) {
-			isAlert=true;
-		}
-
-		//insert an alert in alert table	
-		if (isAlert) {
-			Alert alert = new Alert();
-			alert.setFk_sensor(sensor.getId());
-			alert.setDescription(historic.getMessage());
-			alert.setDate_alert(historic.getDate_signal());
-			alert.setHour_alert(historic.getHour_signal());
-			alert.setFk_user(1);
-			alert.setStatus(true);
-			try {
-				AlertManager alertManager = new AlertManager(connDB);
-				SensorManager sensorManager = new SensorManager(connDB);
-				AlertManager.create(alert);
-				sensor.setStatus(false);
-				SensorManager.update(sensor);
-			}catch(SQLException ex) {
-				System.err.println("Error Cache insert alert or sensor: "+ex.getMessage());
-			}
-		}
-	}
-
-
-
-
-	private static String todayFormatted() {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		return dateFormat.format(new Date());
-	}
-	private static Time todayTime() {
-		return new java.sql.Time(new Date().getTime());
-	}
-
-	//Create static thread to detect broken sensors
-	private static Thread taskBrokenSensors = new Thread(new Runnable() {
-		public void run() {
-			try {//initial delay
-				Thread.sleep(AccessConfig.getINITIAL_DELAY());
-			}catch(Exception ex) {
-				System.err.println(ex.getMessage());
-			}
-			while(true) {
-
-				Connection conBS=null;
-				try {
-					try {
-						conBS = DataSource.getConnection();
-					} catch (SQLException e1) {
-						System.err.println(e1.getMessage());
-					}
-
-					SensorManager sensorManager = new SensorManager(conBS);
-					HistoricalManager historicalManager = new HistoricalManager(conBS);
-					AlertManager alertManager = new AlertManager(conBS);
-
-					ArrayList<Sensor> sensors = new ArrayList<>();
-
-					//get installed and status ok sensors
-					try {
-						sensors = SensorManager.getSensors("select * from sensor where installed=true;");
-					} catch (SQLException | ParseException e) {
-						System.err.println(e.getMessage());
-					}
-					//for each sensor, we check historical table if the last message is superior than 10000 mil of actual hour
-					for (Sensor sensor : sensors) {
-						boolean signal=true;
-						//get signals
-						ArrayList<Historical> hists = new ArrayList<>();
-						try {
-							hists = HistoricalManager.getHistoricals("select * from historical where fk_sensor="+sensor.getId());
-						} catch (SQLException | ParseException e) {
-							System.err.println(e.getMessage());
-						}
-						//create an alert if no hitoricals
-						if(hists.isEmpty()) {
-							signal=false;
-						}else {//create an alert if last historcal high
-							Historical hist = hists.get(hists.size()-1);
-							if (hist.getDate_signal_formatted().equals(todayFormatted())) {
-								if ((new Date().getTime() - (hist.getDate_signal().getTime()+hist.getHour_signal().getTime()))/1000 > AccessConfig.getLAST_SIGNAL_DELAY()) {
-									signal=false;
-								}
-							}
-						}
-						if(!signal) {
-							Alert alert = new Alert();
-							alert.setFk_sensor(sensor.getId());
-							alert.setDescription("no signals");
-							alert.setDate_alert(new Date());
-							alert.setHour_alert(todayTime());
-							alert.setFk_user(1);
-							alert.setStatus(true);
-							try {
-								AlertManager.create(alert);
-								sensor.setStatus(false);
-								SensorManager.update(sensor);
-							}catch(SQLException ex) {
-								System.err.println("Error Cache insert alert or sensor: "+ex.getMessage());
-							}
-						}
-
-					}
-				}finally {
-					//releaseConnection
-					try {
-						DataSource.releaseConnection(conBS);
-					} catch (SQLException e1) {
-						System.err.println(e1.getMessage());
-					}
-				}
-
-				try {//delay
-					Thread.sleep(AccessConfig.getDEFAULT_DELAY());
-				}catch(Exception ex) {
-					System.err.println(ex.getMessage());
-				}
-
-			}
-
-		}
-
-
-	});
+//	private void addToCacheAndTreatment(Historical historic) {
+//		//get sensor of signal
+//		SensorManager sensM = new SensorManager(connDB);
+//		Sensor sensor = null;
+//		boolean isAlert=false;
+//
+//		try {//get the sensor of the signal
+//			sensor = SensorManager.getSensor(historic.getFk_sensor());
+//		} catch (SQLException | ParseException e) {
+//			e.printStackTrace();
+//		}
+//		//add to cache evry signals
+//		if (!CACHE.containsKey(historic.getFk_sensor())) {
+//			ArrayList<Historical> a  = new ArrayList<Historical>();
+//			a.add(historic);
+//			CACHE.put(historic.getFk_sensor(), a);
+//		}else {
+//			ArrayList<Historical> a = CACHE.get(historic.getFk_sensor());
+//			a.add(historic);
+//		}
+//
+//		//check of cache to add alert in DB
+//		ArrayList<Historical> hitoricals = CACHE.get(sensor.getId());//get all signals of the sensor
+//		ArrayList<Historical> last = new ArrayList<>();
+//
+//		//get last signals
+//		for (int i = hitoricals.size()-1; i+1 >= sensor.getFk_type_sensor().getNb_alerts(); i--) {
+//			last.add(hitoricals.get(i));
+//		}
+//
+//		//check if we have a number of signals of alert of alert high
+//		int cpt_alert_signals = 0;
+//		for (int i = 0; i < last.size(); i++) {
+//			if (isAlertInCache(last.get(i), sensor)) {
+//				cpt_alert_signals++;
+//			}
+//		}
+//		if (cpt_alert_signals==sensor.getFk_type_sensor().getNb_alerts()) {
+//			isAlert=true;
+//		}
+//
+//		//insert an alert in alert table	
+//		if (isAlert) {
+//			Alert alert = new Alert();
+//			alert.setFk_sensor(sensor.getId());
+//			alert.setDescription(historic.getMessage());
+//			alert.setDate_alert(historic.getDate_signal());
+//			alert.setHour_alert(historic.getHour_signal());
+//			alert.setFk_user(1);
+//			alert.setStatus(true);
+//			try {
+//				AlertManager alertManager = new AlertManager(connDB);
+//				SensorManager sensorManager = new SensorManager(connDB);
+//				AlertManager.create(alert);
+//				sensor.setStatus(false);
+//				SensorManager.update(sensor);
+//			}catch(SQLException ex) {
+//				System.err.println("Error Cache insert alert or sensor: "+ex.getMessage());
+//			}
+//		}
+//	}
+//
+//
+//
+//
+//	private static String todayFormatted() {
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//		return dateFormat.format(new Date());
+//	}
+//	private static Time todayTime() {
+//		return new java.sql.Time(new Date().getTime());
+//	}
+//
+//	//Create static thread to detect broken sensors
+//	private static Thread taskBrokenSensors = new Thread(new Runnable() {
+//		public void run() {
+//			try {//initial delay
+//				Thread.sleep(AccessConfig.getINITIAL_DELAY());
+//			}catch(Exception ex) {
+//				System.err.println(ex.getMessage());
+//			}
+//			while(true) {
+//
+//				Connection conBS=null;
+//				try {
+//					try {
+//						conBS = DataSource.getConnection();
+//					} catch (SQLException e1) {
+//						System.err.println(e1.getMessage());
+//					}
+//
+//					SensorManager sensorManager = new SensorManager(conBS);
+//					HistoricalManager historicalManager = new HistoricalManager(conBS);
+//					AlertManager alertManager = new AlertManager(conBS);
+//
+//					ArrayList<Sensor> sensors = new ArrayList<>();
+//
+//					//get installed and status ok sensors
+//					try {
+//						sensors = SensorManager.getSensors("select * from sensor where installed=true;");
+//					} catch (SQLException | ParseException e) {
+//						System.err.println(e.getMessage());
+//					}
+//					//for each sensor, we check historical table if the last message is superior than 10000 mil of actual hour
+//					for (Sensor sensor : sensors) {
+//						boolean signal=true;
+//						//get signals
+//						ArrayList<Historical> hists = new ArrayList<>();
+//						try {
+//							hists = HistoricalManager.getHistoricals("select * from historical where fk_sensor="+sensor.getId());
+//						} catch (SQLException | ParseException e) {
+//							System.err.println(e.getMessage());
+//						}
+//						//create an alert if no hitoricals
+//						if(hists.isEmpty()) {
+//							signal=false;
+//						}else {//create an alert if last historcal high
+//							Historical hist = hists.get(hists.size()-1);
+//							if (hist.getDate_signal_formatted().equals(todayFormatted())) {
+//								if ((new Date().getTime() - (hist.getDate_signal().getTime()+hist.getHour_signal().getTime()))/1000 > AccessConfig.getLAST_SIGNAL_DELAY()) {
+//									signal=false;
+//								}
+//							}
+//						}
+//						if(!signal) {
+//							Alert alert = new Alert();
+//							alert.setFk_sensor(sensor.getId());
+//							alert.setDescription("no signals");
+//							alert.setDate_alert(new Date());
+//							alert.setHour_alert(todayTime());
+//							alert.setFk_user(1);
+//							alert.setStatus(true);
+//							try {
+//								AlertManager.create(alert);
+//								sensor.setStatus(false);
+//								SensorManager.update(sensor);
+//							}catch(SQLException ex) {
+//								System.err.println("Error Cache insert alert or sensor: "+ex.getMessage());
+//							}
+//						}
+//
+//					}
+//				}finally {
+//					//releaseConnection
+//					try {
+//						DataSource.releaseConnection(conBS);
+//					} catch (SQLException e1) {
+//						System.err.println(e1.getMessage());
+//					}
+//				}
+//
+//				try {//delay
+//					Thread.sleep(AccessConfig.getDEFAULT_DELAY());
+//				}catch(Exception ex) {
+//					System.err.println(ex.getMessage());
+//				}
+//
+//			}
+//
+//		}
+//
+//
+//	});
 
 
 	public void stopConnection() throws IOException {
